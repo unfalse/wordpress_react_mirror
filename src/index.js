a456450c0fdda11ef47c4ee19e60d9111d37fda7
Iskander Gubaidullin <Iskander_Gubaidullin@epam.com>
2018-06-29 17:24:02 +0300

    first working version

--- src/index.js
+++ src/index.js
@@ -11,26 +11,41 @@ import Post from './components/Post';
 import getPagesAndPosts from './api/postService';
 
 // TODO: add rrm to all links, so links to posts will lead to mirror site
-getPagesAndPosts()
-  .then(posts => {
-    ReactDOM.render(
-      <Router>
-        <App posts={posts}>
-          <Switch>
-            <Route exact path="/"
-              render={() =>
-                <Home posts={ posts.map(p => ({...p, title: <Link to={`/posts/${p.id}`}>{p.title}</Link>}) ) } /> }
-            />
-            <Route exact path="/rrm"
-              render={() => <Home posts={ posts.map(p => ({...p, title: 
-                              <Link to={`/posts/${p.id}`}>{p.title}</Link>}) ) } /> }
-            />
-            <Route path="/posts/:postId"
-              render={m => <Post postData={ posts.find(p => p.id === +m.match.params.postId) }/> }
-            />
-          </Switch>
-        </App>
-      </Router>,
-      document.getElementById('root')
-    );
+getPagesAndPosts('http://unfalsecoding.net')
+  .then(data => {
+    const onChange = (event) => {
+        const url = event.target.value;
+//        localStorage.setItem("currentURL", url);
+        getPagesAndPosts(url)
+            .then(newData => {
+                if (newData.status === 'OK') renderDOM(newData);
+                else renderDOM({ ...data, status: newData.status });
+            });
+    };
+
+    const renderDOM = data => {
+        const { posts } = data;
+        ReactDOM.render(
+            <Router>
+                <App {...data} onChange={onChange}>
+                <Switch>
+                    <Route exact path="/"
+                    render={() =>
+                        <Home posts={ posts.map(p => ({...p, title: <Link to={`/posts/${p.id}`}>{p.title}</Link>}) ) } /> }
+                    />
+                    <Route exact path="/rrm"
+                    render={() => <Home posts={ posts.map(p => ({...p, title: 
+                                    <Link to={`/posts/${p.id}`}>{p.title}</Link>}) ) } /> }
+                    />
+                    <Route path="/posts/:postId"
+                    render={m => <Post postData={ posts.find(p => p.id === +m.match.params.postId) }/> }
+                    />
+                </Switch>
+                </App>
+            </Router>,
+            document.getElementById('root')
+        );
+    };
+
+    renderDOM(data);
   });
